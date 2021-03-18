0d4b6b52ac57bb439a308758fb6c7a39a4072e0d
NY <>
2021-03-18 12:24:05 +0800

    4. Patch Session Cookie

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Provider>
+    <Provider session={pageProps.session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
